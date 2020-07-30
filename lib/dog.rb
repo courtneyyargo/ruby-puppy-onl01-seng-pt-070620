c619f513981fc4e17175f7924dadbd6de11d4c67
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-30 19:23:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,32 +1,31 @@
 class Dog
-attr_accessor :name
+   @@all = []
 
-@@all = []
-def initialize(name)
-  @name = name 
-  @@all << self 
-end 
+    def initialize(name)
+     @name = name
+     self.save
+   end
 
-def self.all 
-  @@all
-end 
+    def self.all
+     @@all
+   end
 
-def self.clear_all
-  @@all = []
-end 
+    def self.clear_all
+     @@all = [] 
+   end
 
-def self.print_all
-  @@all.each do |dog|
-    puts dog.name
-  end
+    def self.print_all
+     @@all.each do |dog|
+       puts dog.name
+     end
+ end
+
+    def save
+     @@all << self
+   end
+
+    def name
+     @name
+   end
+
   end 
-  
-def save 
-  @@all << self 
-end 
-
-dog name
-@name 
-end 
-
-end
